9c7ca894eb21e3ef771a5b128f2283c02a31180f
이효원 <ymj07168@sookmyung.ac.kr>
2022-05-14 02:46:20 +0900

    2022/05/14 4주차 orm

--- first-django/firstdjango/settings.py
+++ first-django/firstdjango/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'first',
     'second',
+    'third',
 ]
 
 MIDDLEWARE = [
--- first-django/firstdjango/urls.py
+++ first-django/firstdjango/urls.py
@@ -24,5 +24,6 @@ from first import views
 urlpatterns = [
     path('first/', include('first.urls')),
     path('second/', include('second.urls')),
+    path('third/', include('third.urls')),
     path('admin/', admin.site.urls),
 ]
