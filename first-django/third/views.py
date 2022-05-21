6ea908b0d3f90c83865c80a621e868046b7175f0
이효원 <ymj07168@sookmyung.ac.kr>
2022-05-21 14:06:44 +0900

    2022/05/14 4주차 orm

--- first-django/third/views.py
+++ first-django/third/views.py
@@ -1,8 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from third.models import Restaurant
+from django.core.paginator import Paginator
+from third.forms import RestaurantForm
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
 def list(request):
-    context = {
+    restaurants = Restaurant.objects.all()
+    paginator = Paginator(restaurants, 5)
 
+    page = request.GET.get('page')  ## third/list?page=1
+    items = paginator.get_page(page)
+
+    context = {
+        'restaurants': items
     }
     return render(request, 'third/list.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        form = RestaurantForm(request.POST)
+        if form.is_valid():
+            new_item = form.save()
+
+        return HttpResponseRedirect('/third/list/')
+    form = RestaurantForm()
+    return render(request, 'third/create.html', {'form': form})
+
+
+def update(request):
+    if request.method == 'POST' and 'id' in request.POST:
+        # item = Restaurant.objects.get(pk=request.POST.get('id'))
+        item = get_object_or_404(Restaurant, pk=request.POST.get('id'))
+        form = RestaurantForm(request.POST, instance=item)
+        if form.is_valid():
+            item = form.save()
+    elif request.method == 'GET':
+        # item = Restaurant.objects.get(pk=request.GET.get('id'))
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
+        form = RestaurantForm(instance=item)
+        return render(request, 'third/update.html', {'form': form})
+    return HttpResponseRedirect('/third/list/')
+
+def detail(request):
+    if 'id' in request.GET:
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
+        return render(request, 'third/detail.html', {'item': item})
+    return HttpResponseRedirect('/third/list/')
+
+def delete(request):
+    if 'id' in request.GET:
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
+        item.delete()
+    return HttpResponseRedirect('/third/list/')
